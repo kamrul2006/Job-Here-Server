bb17704405f42fbd35cf9b3b4660cdcf71bbaf4a
Kamrul Islam Apurba <kamrulislamapurba@gmail.com>
2024-12-16 01:38:46 +0600

    Apply add,getall,getByID  done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
 const app = express();
@@ -27,7 +27,7 @@ async function run() {
 
         // ---------------creating DATA BASE ON MONGO DB----------------------
         const JobCollection = client.db("JobDB").collection('All-Jobs')
-        // const UserCollection = client.db("JobDB").collection('All-Users')
+        const ApplyCollection = client.db("JobDB").collection('All-Apply')
 
 
         //---------------------------Showing all jobs------------------------
@@ -38,15 +38,59 @@ async function run() {
         })
 
         //-------------------add Job-------------------------------------------
-        app.post('/Jobs', async (req, res) => {
+        app.post('/allJobs', async (req, res) => {
             const newJob = req.body
-            console.log(newJob)
+            // console.log(newJob)
             const result = await JobCollection.insertOne(newJob)
+            res.send(result)
+        })
+
+        //---------------------------Get Job by ID-----------------------------
+        app.get('/allJobs/:id', async (req, res) => {
+            const id = req.params.id
+            // console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await JobCollection.findOne(query)
             res.send(result)
         })
 
 
 
+
+        //-------------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------------
+
+
+
+        //-------------------add Apply-----------------------------------------
+        app.post('/apply', async (req, res) => {
+            const newApply = req.body
+            //  console.log(newVisa)
+            const result = await ApplyCollection.insertOne(newApply)
+            res.send(result)
+        })
+
+        //---------------------------Showing all Apply------------------------
+        app.get('/apply', async (req, res) => {
+            //------------------------------Search here---------------------------
+            const { Search } = req.query;
+            let option = {}
+            if (Search) {
+                option = { countryName: { $regex: Search, $options: "i" } }
+            }
+            const cursor = ApplyCollection.find(option)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        //---------------------------Get apply by ID-----------------------
+        app.get('/apply/:id', async (req, res) => {
+            const id = req.params.id
+            //  console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await ApplyCollection.findOne(query)
+            res.send(result)
+        })
 
 
 
