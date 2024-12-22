ad2319f61581a6d841356242945ef690f5a3b2ba
Kamrul Islam Apurba <kamrulislamapurba@gmail.com>
2024-12-23 03:11:51 +0600

    Des 24

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-require('dotenv').config()
+const cookieParser = require('cookie-parser')
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000
 
 
 // --------------------middle ware-------------------------
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
 app.use(express.json());
+app.use(cookieParser());
+
+// ------------------------------------------
+const logger = (req, res, next) => {
+    console.log('inside the logger')
+    next()
+}
+
+const tokenVerify = (req, res, next) => {
+    const token = req?.cookies?.token
+    console.log('inside the tokenVerify part')
+
+    if (!token) {
+        return res.status(401).send({ massage: "Unauthorized access" })
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            console.error('JWT Verification Error:', err.message);
+            return res.status(403).send({ message: "Forbidden: Invalid or Expired Token" });
+        }
+        req.user = decoded;
+        next();
+    });
+
+}
 
 // ----------------connecting MONGO db---------------------------
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8jqou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,9 +59,23 @@ async function run() {
         const JobCollection = client.db("JobDB").collection('All-Jobs')
         const ApplyCollection = client.db("JobDB").collection('All-Apply')
 
+        // ---auth related Apis-------------------------------
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1h' })
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+                })
+                // .send(token)
+                .send({ success: true })
+        })
+
 
         //---------------------------Showing all jobs------------------------
         app.get('/allJobs', async (req, res) => {
+            // console.log('inside all jobs')
             const cursor = JobCollection.find()
             const result = await cursor.toArray()
             res.send(result)
@@ -99,16 +143,21 @@ async function run() {
         })
 
         //---------------------------Showing all Apply------------------------
-        app.get('/apply', async (req, res) => {
-            //------------------------------Search here---------------------------
-            const { Search } = req.query;
-            let option = {}
-            if (Search) {
-                option = { countryName: { $regex: Search, $options: "i" } }
+        app.get('/apply', tokenVerify, async (req, res) => {
+            const QEmail = req.query.email
+            const query = { email: QEmail }
+
+            console.log(QEmail)
+            // console.log(req.user.email.email)
+
+            if (req.user.email.email !== req.query.email) {
+                return res.status(403).send({ massage: "forbidden" })
             }
-            const cursor = ApplyCollection.find(option)
-            const result = await cursor.toArray()
-            res.send(result)
+
+            const cursor = await ApplyCollection.find(query).toArray()
+            // const result = cursor
+            // console.log(cursor)
+            res.send(cursor)
         })
 
         //---------------------------Get apply by ID-----------------------
